6b4f164c675463306f48636227b452a2b6136be7
Beomjin <qjawls646@gmail.com>
2025-06-01 17:03:04 +0900

    findOne 구현 후 모듈에서 export

--- src/part/part.module.ts
+++ src/part/part.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PartService } from './part.service';
 import { PartController } from './part.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Part } from './part.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Part])],
   providers: [PartService],
-  controllers: [PartController]
+  controllers: [PartController],
+  exports: [PartService]
 })
 export class PartModule {}
--- src/part/part.service.ts
+++ src/part/part.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Part } from './part.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PartService {}
+export class PartService {
+    constructor(
+        @InjectRepository(Part) 
+        private readonly partRepository: Repository<Part> ) 
+    {}
+
+    findOne(id:number): Promise< Part | null > {
+        return this.partRepository.findOne({
+            where: {
+                id
+            }
+        })
+    }
+}
